bf8dba180aeddee4334569e9415054a90ef98f29
gr-am-alex <graham.ga256@gmail.com>
2026-05-03 11:21:53 +0530

    Rename bus_reservation.sql to bus_reservation(Harsh).sql

---
+++ database/bus_reservation(Harsh).sql
@@ -0,0 +1,256 @@
+﻿-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: BusReservationSystem
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bookings`
+--
+
+DROP TABLE IF EXISTS `bookings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookings` (
+  `booking_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `schedule_id` int DEFAULT NULL,
+  `seat_id` int DEFAULT NULL,
+  `pnr` varchar(6) DEFAULT NULL,
+  `total_fee` decimal(6,2) DEFAULT NULL,
+  PRIMARY KEY (`booking_id`),
+  UNIQUE KEY `pnr` (`pnr`),
+  KEY `user_id` (`user_id`),
+  KEY `schedule_id` (`schedule_id`),
+  KEY `seat_id` (`seat_id`),
+  CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`schedule_id`),
+  CONSTRAINT `bookings_ibfk_3` FOREIGN KEY (`seat_id`) REFERENCES `seats` (`seat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookings`
+--
+
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+INSERT INTO `bookings` VALUES (1,1,1,1,'548921',450.00),(2,2,2,8,'784512',700.00);
+/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `buses`
+--
+
+DROP TABLE IF EXISTS `buses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `buses` (
+  `bus_id` int NOT NULL AUTO_INCREMENT,
+  `bus_number` varchar(20) DEFAULT NULL,
+  `bus_type` enum('AC','Non-AC') DEFAULT NULL,
+  `total_seats` int DEFAULT NULL,
+  PRIMARY KEY (`bus_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buses`
+--
+
+LOCK TABLES `buses` WRITE;
+/*!40000 ALTER TABLE `buses` DISABLE KEYS */;
+INSERT INTO `buses` VALUES (1,'KL01AB1234','AC',40),(2,'KL07CD5678','Non-AC',45),(3,'KL09EF9012','AC',36);
+/*!40000 ALTER TABLE `buses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `route_stops`
+--
+
+DROP TABLE IF EXISTS `route_stops`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `route_stops` (
+  `route_id` int NOT NULL,
+  `stop_id` int NOT NULL,
+  `stop_order` int DEFAULT NULL,
+  `distance_from_start` decimal(6,2) DEFAULT NULL,
+  PRIMARY KEY (`route_id`,`stop_id`),
+  KEY `stop_id` (`stop_id`),
+  CONSTRAINT `route_stops_ibfk_1` FOREIGN KEY (`route_id`) REFERENCES `routes` (`route_id`),
+  CONSTRAINT `route_stops_ibfk_2` FOREIGN KEY (`stop_id`) REFERENCES `stops` (`stop_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `route_stops`
+--
+
+LOCK TABLES `route_stops` WRITE;
+/*!40000 ALTER TABLE `route_stops` DISABLE KEYS */;
+INSERT INTO `route_stops` VALUES (1,1,1,0.00),(1,2,2,60.00),(1,3,3,120.00),(1,4,4,150.00),(2,2,2,120.00),(2,3,3,180.00),(2,5,1,0.00),(2,6,4,360.00),(3,2,1,0.00),(3,3,2,60.00),(3,7,3,380.00);
+/*!40000 ALTER TABLE `route_stops` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `routes`
+--
+
+DROP TABLE IF EXISTS `routes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `routes` (
+  `route_id` int NOT NULL AUTO_INCREMENT,
+  `route_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`route_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `routes`
+--
+
+LOCK TABLES `routes` WRITE;
+/*!40000 ALTER TABLE `routes` DISABLE KEYS */;
+INSERT INTO `routes` VALUES (1,'Palakkad - Kochi'),(2,'Kozhikode - Trivandrum'),(3,'Thrissur - Bangalore');
+/*!40000 ALTER TABLE `routes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schedules`
+--
+
+DROP TABLE IF EXISTS `schedules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `schedules` (
+  `schedule_id` int NOT NULL AUTO_INCREMENT,
+  `bus_id` int DEFAULT NULL,
+  `route_id` int DEFAULT NULL,
+  `departure_time` datetime DEFAULT NULL,
+  `fare` decimal(6,2) DEFAULT NULL,
+  PRIMARY KEY (`schedule_id`),
+  KEY `bus_id` (`bus_id`),
+  KEY `route_id` (`route_id`),
+  CONSTRAINT `schedules_ibfk_1` FOREIGN KEY (`bus_id`) REFERENCES `buses` (`bus_id`),
+  CONSTRAINT `schedules_ibfk_2` FOREIGN KEY (`route_id`) REFERENCES `routes` (`route_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schedules`
+--
+
+LOCK TABLES `schedules` WRITE;
+/*!40000 ALTER TABLE `schedules` DISABLE KEYS */;
+INSERT INTO `schedules` VALUES (1,1,1,'2026-04-25 08:00:00',450.00),(2,2,2,'2026-04-25 10:30:00',700.00),(3,3,3,'2026-04-25 21:00:00',1200.00);
+/*!40000 ALTER TABLE `schedules` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seats`
+--
+
+DROP TABLE IF EXISTS `seats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seats` (
+  `seat_id` int NOT NULL AUTO_INCREMENT,
+  `bus_id` int DEFAULT NULL,
+  `seat_type` enum('Window','Aisle','Middle') DEFAULT NULL,
+  `seat_no` varchar(5) DEFAULT NULL,
+  `restriction` enum('None','Women','PWD') DEFAULT NULL,
+  `seat_status` enum('Available','Booked') DEFAULT 'Available',
+  PRIMARY KEY (`seat_id`),
+  KEY `bus_id` (`bus_id`),
+  CONSTRAINT `seats_ibfk_1` FOREIGN KEY (`bus_id`) REFERENCES `buses` (`bus_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seats`
+--
+
+LOCK TABLES `seats` WRITE;
+/*!40000 ALTER TABLE `seats` DISABLE KEYS */;
+INSERT INTO `seats` VALUES (1,1,'Window','A1','None','Available'),(2,1,'Aisle','A2','None','Available'),(3,1,'Window','A3','Women','Available'),(4,1,'Aisle','A4','None','Available'),(5,1,'Window','B1','None','Available'),(6,1,'Aisle','B2','None','Available'),(7,2,'Window','A1','None','Available'),(8,2,'Aisle','A2','None','Available'),(9,2,'Window','A3','Women','Available'),(10,2,'Aisle','A4','None','Available'),(11,3,'Window','A1','None','Available'),(12,3,'Aisle','A2','None','Available'),(13,3,'Window','A3','None','Available'),(14,3,'Aisle','A4','None','Available');
+/*!40000 ALTER TABLE `seats` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stops`
+--
+
+DROP TABLE IF EXISTS `stops`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stops` (
+  `stop_id` int NOT NULL AUTO_INCREMENT,
+  `stop_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`stop_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stops`
+--
+
+LOCK TABLES `stops` WRITE;
+/*!40000 ALTER TABLE `stops` DISABLE KEYS */;
+INSERT INTO `stops` VALUES (1,'Palakkad'),(2,'Thrissur'),(3,'Ernakulam'),(4,'Kochi'),(5,'Kozhikode'),(6,'Trivandrum'),(7,'Bangalore');
+/*!40000 ALTER TABLE `stops` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `gender` enum('Male','Female','Other') DEFAULT NULL,
+  `pwd_status` tinyint(1) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Arjun Menon','Male',0,'arjun@gmail.com','1234','9876543210'),(2,'Ananya Nair','Female',0,'ananya@gmail.com','1234','9123456780'),(3,'Rahul Das','Male',0,'rahul@gmail.com','1234','9988776655'),(4,'Meera Krishna','Female',1,'meera@gmail.com','1234','9090909090');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-24 17:27:36
